c3813fa91d7fa2bfce0be66fdf4186b11dc0c037
Kostrvba <kostrubiec50@gmail.com>
2023-03-28 16:40:50 +0200

    login, register and upgraded main page

--- GoodHands/views.py
+++ GoodHands/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.views import View
-from GoodHands.models import Donation, Institution
+from GoodHands.models import Donation, Institution, User
 
 
 class LandingPage(View):
@@ -8,7 +9,12 @@ class LandingPage(View):
     def get(self, request):
         donations = Donation.objects.all().count()
         organisations = Institution.objects.all().count()
-        return render(request, 'index.html', {'donations': donations, 'organisations': organisations})
+        foundations = Institution.objects.filter(type=Institution.FUNDACJA)
+        organs = Institution.objects.filter(type=Institution.ORGANIZACJA_POZARZADOWA)
+        locals = Institution.objects.filter(type=Institution.ZBIORKA_LOKALNA)
+        return render(request, 'index.html',
+                      {'donations': donations, 'organisations': organisations, 'foundations': foundations,
+                       'organs': organs, 'locals': locals})
 
 
 class AddDonation(View):
@@ -22,8 +28,31 @@ class Login(View):
     def get(self, request):
         return render(request, 'login.html')
 
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        return redirect('register')
 
 class Register(View):
 
     def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        name = request.POST['name']
+        surname = request.POST['surname']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            user = User.objects.create_user(username=email, email=email)
+            user.set_password(password)
+            user.first_name = name
+            user.last_name = surname
+            user.save()
+            return redirect('login')
         return render(request, 'register.html')
